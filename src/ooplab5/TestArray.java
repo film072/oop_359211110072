779456a160410174631e7d55d0cb753e007c9fca
LAB_COM-MT_1 <krittiyapornkhaikhao14@gmail.com>
2017-12-13 09:11:53 +0700

    testArray

---
+++ src/ooplab5/TestArray.java
@@ -0,0 +1,44 @@
+package ooplab5;
+//  introduction to array in java
+
+public class TestArray {
+    public static void main (String[] args){
+        //Array
+        int number[];
+        number = new int[5];
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+        System.out.println(number[2]);
+        System.out.println(number[3]);
+        System.out.println(number[4]);
+        number[1] = 10;
+        System.out.println(number[1]);
+        number[4] = 20;
+        number[3] = number[1] + number[4];
+        System.out.println(number[3]);
+        System.out.println(number[4]);
+
+        int total = o;
+        for (int i =0;i<number.length;i++) {
+
+            for (int i = 0; i < number.length; i++)
+                System.out.println("number[" + i + "]: " + number[i]);
+            total += number[i]; //total = total+number[i]
+        }//for
+        System.out.println("total in array is "+total);
+
+
+          double value [] = new double[5];
+          for (int i =0;<value.length;i++)
+              System.out.println(msg[i]);
+
+
+
+
+
+
+
+    }//main
+
+
+}//class
